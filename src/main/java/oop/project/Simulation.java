5fef143f1def9dc0dc2668c4eab47bbe44f35647
Devedir <109344310+Devedir@users.noreply.github.com>
2024-01-17 13:27:09 +0100

    Simulation template

--- src/main/java/oop/project/Simulation.java
+++ src/main/java/oop/project/Simulation.java
@@ -4,10 +4,11 @@ import oop.project.Settings.AnimalSettings;
 import oop.project.Settings.MapSettings;
 import oop.project.model.WorldMap;
 
-public class Simulation { // TODO
-    WorldMap worldMap;
-    MapSettings mapSettings;
-    AnimalSettings animalSettings;
+public class Simulation implements Runnable { // TODO
+    private final WorldMap worldMap;
+    private final MapSettings mapSettings;
+    private final AnimalSettings animalSettings;
+    private int day;
 
     public Simulation(MapSettings mapSettings, AnimalSettings animalSettings) {
         this.mapSettings = mapSettings;
@@ -15,7 +16,46 @@ public class Simulation { // TODO
         worldMap = new WorldMap(mapSettings);
     }
 
+    @Override
     public void run() {
         System.out.println("Start!");
+        initializeSimulation();
+        day = 1;
+        visualize();
+        while (day < 10) { // Warunek tymczasowy
+            removeDeadAnimals();
+            moveAllAnimals();
+            eatPlants();
+            breedAnimals();
+            growPlants();
+            visualize();
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.err.println("Symulacja przerwana: " + e.getMessage());
+            }
+            day++;
+        }
+    }
+
+    private void initializeSimulation() { // TODO
+    }
+
+    private void removeDeadAnimals() { // TODO
+    }
+
+    private void moveAllAnimals() { // TODO
+    }
+
+    private void eatPlants() { // TODO
+    }
+
+    private void breedAnimals() { // TODO
+    }
+
+    private void growPlants() { // TODO
+    }
+
+    private void visualize() { // TODO
     }
 }
