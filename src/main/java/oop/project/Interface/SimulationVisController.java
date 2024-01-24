8ba17d7d904f3824a8f8fdc6ba368488b8aba741
Rafał Szepieniec <rszepieniec@student.agh.edu.pl>
2024-01-24 17:22:57 +0100

    nowy plik fxml i pare innych rzeczy

--- src/main/java/oop/project/Interface/SimulationVisController.java
+++ src/main/java/oop/project/Interface/SimulationVisController.java
@@ -1,13 +1,32 @@
 package oop.project.Interface;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SimulationVisController implements Initializable {
+    @FXML
+    public GridPane mapGridPane;
+    @FXML
+    public Label titleOfStatistics;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void buttonStartStopFunc(ActionEvent actionEvent) {
+    }
+
+    public void showDominantGenotypeFunc(ActionEvent actionEvent) {
+    }
+
+    public void showPreferredSpaceIdFunc(ActionEvent actionEvent) {
     }
 }
